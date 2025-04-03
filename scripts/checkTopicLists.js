83c135f18b0d1369b5822e4cc91078168621919e
Samuel Hess <sam.hess@me.com>
2025-04-03 15:27:09 +0300

    update word topics

--- scripts/checkTopicLists.js
+++ scripts/checkTopicLists.js
@@ -4,70 +4,40 @@ import {readFile} from 'fs/promises'
 const contents = await readFile('./data/topicLists.json')
 const entries = JSON.parse(contents)
 
-for (const [index,entry] of entries.entries()) {
-  const {word, topic:topicName} = entry
-  if (word.includes('(') || !word.endsWith(')')) {
-    const record = {partOfSpeech:'n', lang:'eng'}
-    record.topic = await db.topic.findFirst({where:{name:{contains:topicName}}})
-    record.term = word
-      .replace(/\s-\s/,'-')
-      .replace(/\( adj\)/,'(adj)')
-      .replace('bunch (of','bunch (of bananas)')
-      .replace('spicy bananas)','spicy')
-      .replace('enter (a','enter (a competition)')
-      .replace('channel (with','channel (with TV)')
-      .replace('officer ( e.g.','officer (e.g. prison/police)')
-      .replace('laptop','laptop (computer)')
-      .replace('(computer)','computer programmer')
-      
-      
-    if (record.term.endsWith('(n)')) record.partOfSpeech = 'n'
-    else if (record.term.endsWith('(v)')) record.partOfSpeech = 'v'
-    else if (record.term.endsWith('(phr v)')) record.partOfSpeech = 'phr v'
-    else if (record.term.endsWith('(n & v)')) record.partOfSpeech = 'n & v'
-    else if (record.term.endsWith('(v & adj)')) record.partOfSpeech = 'v & adj'
-    else if (record.term.endsWith('(adj & n)')) record.partOfSpeech = 'adj & n'
-    else if (record.term.endsWith('(adj)')) record.partOfSpeech = 'adj'
-    else if (record.term.endsWith('(adv)')) record.partOfSpeech = 'adv'
+function fixTopic(topic) {
+  return topic==='Home'?'House and Home':topic
+}
 
-    else if (record.term.endsWith('(Am Eng)')) record.lang = 'ena'
-    else if (record.term.endsWith('(Br Eng)')) record.lang = 'enb'
+function fixTerm(term) {
+  return term
+    .replace(/\s-\s/,'-')
+    .replace('bunch (of','bunch (of bananas)')
+    .replace('spicy bananas)','spicy')
+    .replace('enter (a','enter (a competition)')
+    .replace('channel (with','channel (with TV)')
+    .replace('officer ( e.g.','officer (e.g. prison/police)')
+    .replace('laptop','laptop (computer)')
+    .replace('(computer)','computer programmer')
+    .replace(/\s\([\w\s&]*\)$/,'')
+}
 
-    record.term = record.term
-      .replace(' (n)','')
-      .replace(' (v)','')
-      .replace(' (phr v)','')
-      .replace(' (n & v)','')
-      .replace(' (v & adj)','')
-      .replace(' (adj & n)','')
-      .replace(' (adj)','')
-      .replace(' (adv)','')
-      .replace(' (Am Eng)','')
-      .replace(' (Br Eng)','')
-  
-    //else if (record.term.endsWith(')')) console.log(record.term)
-    if (record.topic) {
-      const {term,lang:language} = record
-      const exists = await db.word.findFirst({
-        where:{
-          term,
-          language
-        }
+for (const entry of entries) {
+  entry.topic = fixTopic(entry.topic)
+  const topic = await db.topic.findFirst({where:{name:{startsWith:entry.topic}}})
+  if (topic) {
+    const term = fixTerm(entry.word)
+    const word = await db.word.findFirst({
+      where:{term,language:{in:['eng','ena','enb']}}})
+    if (word) {
+      const {language,partOfSpeech} = word
+      await db.word.update({
+        where: {term_partOfSpeech_language: {term, partOfSpeech, language}},
+        data: {Topic:{connect:{key:topic.key}}}
       })
-      if (exists) {
-        const {partOfSpeech} = exists
-        await db.word.update({
-          where: {term_partOfSpeech_language: {term, partOfSpeech, language}},
-          data: {
-            Topic: {connect:{key:record.topic.key}}
-          }
-        })
-      }
-      else {
-        console.log(`${term} not found ${language}`)
-      }
-    } else if (!record.topic) {
-      console.log(`${record.term} has no topic`)
+    } else {
+      console.log(`${entry.word}`)
     }
+  } else {
+    console.log(`${entry.topic} not found`)
   }
 }
